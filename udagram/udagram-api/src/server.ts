f0cc48f303e0d6272b06f69474a5746ca74b8d5b
abdulaziz1928 <abdulaziz1928@gmail.com>
2023-03-18 12:29:56 +0300

    small fix

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
 import { sequelize } from "./sequelize";
-
+import { config } from "./config/config";
 import { IndexRouter } from "./controllers/v0/index.router";
 
 import bodyParser from "body-parser";
@@ -25,7 +25,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = config.port;
 
   app.use(bodyParser.json());
 
